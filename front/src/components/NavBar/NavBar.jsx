ef088a6561e0227fa98241469b469724016e2776
Jorge Miguel Semerena Nñez <mrsemerena@gmail.com>
2023-02-02 12:56:04 -0600

    Back and front Folders
    Almost complete, just the file app.js from the back/stc/app.jss is not written, the file is empty

---
+++ front/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import style from "./NavBar.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
+
+const NavBar = ({ onSearch }) => {
+    return(
+        <nav className={style.nav} >
+            <div className={style.btns}>
+                <Link to='/' className={style.about} >LOGOUT</Link>
+                <Link to='/about' className={style.about} >About</Link>
+                <Link to='/home' className={style.home} >Home</Link>
+                <Link to='/favorites' className={style.about} >Favorites</Link>
+            </div>
+
+            <SearchBar onSearch={onSearch} />
+        </nav>
+    )
+}
+
+export default NavBar;
